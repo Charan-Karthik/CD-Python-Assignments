01a8755de4acab0473abe691a3c2504bf93bfb31
Charan Karthik <secharank@gmail.com>
2022-07-06 13:54:04 -0700

    push to save changes

---
+++ Fundamentals/OOP/basketball_dictionaries.py
@@ -0,0 +1,77 @@
+class Player:
+    def __init__(self, dictionary):
+        self.name = dictionary["name"]
+        self.age = dictionary["age"]
+        self.position = dictionary["position"]
+        self.team = dictionary["team"]
+    
+    def display_player_info(self):
+        print("Name:", self.name)
+        print("age:", self.age)
+        print("position:", self.position)
+        print("team:", self.team)
+        print()
+
+kevin = {
+        "name": "Kevin Durant", 
+        "age":34, 
+        "position": "small forward", 
+        "team": "Brooklyn Nets"
+}
+jason = {
+        "name": "Jason Tatum", 
+        "age":24, 
+        "position": "small forward", 
+        "team": "Boston Celtics"
+}
+kyrie = {
+        "name": "Kyrie Irving", 
+        "age":32, "position": "Point Guard", 
+        "team": "Brooklyn Nets"
+}
+
+player1 = Player(kevin)
+player2 = Player(jason)
+player3 = Player(kyrie)
+
+player1.display_player_info()
+player2.display_player_info()
+player3.display_player_info()
+
+players = [
+    {
+    	"name": "Kevin Durant", 
+    	"age":34, 
+    	"position": "small forward", 
+    	"team": "Brooklyn Nets"
+    },
+    {
+    	"name": "Jason Tatum", 
+    	"age":24, 
+    	"position": "small forward", 
+    	"team": "Boston Celtics"
+    },
+    {
+    	"name": "Kyrie Irving", 
+    	"age":32, "position": "Point Guard", 
+    	"team": "Brooklyn Nets"
+    },
+    {
+    	"name": "Damian Lillard", 
+    	"age":33, "position": "Point Guard", 
+    	"team": "Portland Trailblazers"
+    },
+    {
+    	"name": "Joel Embiid", 
+    	"age":32, "position": "Power Foward", 
+    	"team": "Philidelphia 76ers"
+    },
+    {
+    	"name": "", 
+    	"age":16, 
+    	"position": "P", 
+    	"team": "en"
+    }
+]
+
+# still working on challenge 3 and NINJA bonus
