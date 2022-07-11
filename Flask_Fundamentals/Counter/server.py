ba94c7147e317e9acef1cdf942c328c2a5e6154e
Charan Karthik <secharank@gmail.com>
2022-07-10 23:16:19 -0700

    counter

---
+++ Flask_Fundamentals/Counter/server.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, redirect, session
+app = Flask(__name__)
+app.secret_key = "javascript"
+
+@app.route('/')
+def display_counter():
+    if 'count' not in session:
+        session['count'] = 1
+    else:
+        session['count'] += 1
+    return render_template("index.html")
+
+@app.route("/addtwo", methods=['post'])
+def addtwo():
+    session['count'] += 1
+    return redirect('/')
+
+@app.route("/destroy_session", methods=['post'])
+def clear_sesh():
+    session.clear()
+    return redirect('/')
+
+if __name__=="__main__":
+    app.run(debug=True, port=5000)
